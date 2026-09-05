3f5b713aa8b24f988219bbad493d77513c88bcb3
ayoolaowoilu <ayoolaowoilu@gmail.com>
2026-09-05 16:04:07 +0100

    Chore : Refactored the ui of the session page

--- app/session/[id]/page.tsx
+++ app/session/[id]/page.tsx
@@ -519,7 +519,7 @@ function ManagerView({
                 <line x1="16" y1="17" x2="8" y2="17" />
                 <polyline points="10 9 9 9 8 9" />
               </svg>
-              PDF
+              Download Attendance PDF
             </motion.button>
             {session.status === "active" && (
               <motion.button
